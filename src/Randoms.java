aab59e6ad776adcd577fbdc001c1fd9dfd87bb62
Mikhail Baranovskii <bm_y@mail.ru>
2022-07-08 16:05:38 +0300

    Initial commit

---
+++ src/Randoms.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+//  (max - min + 1) = (100 - 90 + 1) = 11, чтобы включить верхнюю границу (10-ку) в рандом. (0...10)
+            }
+        };
+    }
+
+}
